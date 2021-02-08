5108906437656e43c2b262bf3df2b9468092084a
aftab <aftabsaboorkhan@gmail.com>
2021-02-08 10:29:11 +0530

    Updated File

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express()
 
 mongoose.connect('mongodb://localhost/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
-})
+});
+
+
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -16,14 +18,17 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
+  await ShortUrl.create({ full: req.body.fullUrl });
+
 
   res.redirect('/')
 })
 
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  if (shortUrl == null) return res.sendStatus(404);
+
+ 
 
   shortUrl.clicks++
   shortUrl.save()
@@ -31,4 +36,6 @@ app.get('/:shortUrl', async (req, res) => {
   res.redirect(shortUrl.full)
 })
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000,()=>{
+  console.log("Server started");
+});
